2740e55fd6f2d480ae8e06b1f6981f7c72b96125
HerboSpringston <herbospringston@gmail.com>
2021-07-21 12:31:41 -0500

    Added getRandomCards() and addFunds(). Added new buttons to UI. Fixed mobile layout.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let firstCard = 0;
-let secondCard = 0;
-let sum = 0;
+let funds = 0;
+let cards = [];
+let sum;
 let blackJackStatus = false;
 let message = "";
 let messageElement = document.getElementById("message-el");
@@ -8,42 +8,37 @@ let sumElement = document.getElementById("sum-el")
 let cardsElement = document.getElementById("cards-el");
 let errorMessage = document.getElementById("error-message");
 let startGameButton = document.getElementById("start-game-btn");
+let moneyAmountElement = document.getElementById("money-amount");
 
 function startGame() {
-    firstCard = Math.floor(Math.random() * 12);
-    secondCard = Math.floor(Math.random() * 12);
-
-    while (firstCard === 0) {
-        firstCard = Math.floor(Math.random() * 12);
-    }
-
-    while (secondCard === 0) { 
-        secondCard = Math.floor(Math.random() * 12);
-    }
-
-    sum = firstCard + secondCard;
-
+    cards[0] = getRandomCard();
+    cards[1] = getRandomCard();
+    sum = cards[0] + cards[1];
     checkBlackJackStatus(sum);
-   
-    cardsElement.textContent = "Cards: " + firstCard + ", " + secondCard;
-
+    cardsElement.textContent = "Cards: " + cards[0] + ", " + cards[1];
     sumElement.textContent = "Sum: " + sum;
-
     startGameButton.textContent = "New Game?"; 
+    errorMessage.textContent = "";
 }
 
 function newCard() {
-    if (sum < 21) {
-        let newCard = Math.floor(Math.random() * 12);
+    if (sum == undefined) {
+        errorMessage.textContent = "Start game.";
+    }
+    else if (sum < 21) {
+        let newCard = getRandomCard();
         while (newCard === 0) { 
-            newCard = Math.floor(Math.random() * 12);
+            newCard = getRandomCard();
         }
+        cards.push(newCard);
         sum += newCard;
         cardsElement.textContent += ", " + newCard;
         sumElement.textContent = "Sum: " + sum;
         checkBlackJackStatus(sum);
+    } else if (sum === 21){
+        errorMessage.textContent = "Blackjack achieved.";
     } else {
-        errorMessage.textContent = "You are out.";
+        errorMessage.textContent = "You are out.";      
     }
 }
 
@@ -56,9 +51,27 @@ function checkBlackJackStatus(sum) {
         message = "You've got Blackjack.";
         messageElement.textContent = message;
     } else {
-        isAlive = false;
         message = "You're out of the game.";
         messageElement.textContent = message;
     }
+}
+
+function getRandomCard() {
+    let value = Math.floor(Math.random() * 13 + 1);
+    if (value === 1) {
+        return 11;
+    } else if (value > 10) {
+        return 10;
+    } else {
+        return value;
+    }
+}
+
+function addFunds() {
+    fundsToAdd = prompt("How much to add? ");
+    fundsToAdd = Number(fundsToAdd);
+    funds += fundsToAdd;
+    console.log(funds);
+    moneyAmountElement.textContent = "Funds: $" + funds;
 }
 
